5528defc6c3ede729b5264939985e09821eb55f9
RudyMasGoNext <rudy.mas@go-next.be>
2025-09-22 15:48:15 +0200

    Added menu page for forms

--- src/controllers/form_viewer/FormViewerController.php
+++ src/controllers/form_viewer/FormViewerController.php
@@ -2,6 +2,9 @@
 
 namespace Controller\form_viewer;
 
+use Controller\TilesOnly;
+use Repository\FormsRepo;
+use Repository\FormViewerFormAccessRepo;
 use Service\FormViewerService;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -38,8 +41,30 @@ class FormViewerController
     {
         $formViewerSettings = FormViewerService::checkAccess();
 
+        $formViewerFromAccess = new FormViewerFormAccessRepo();
+        $formViewerFromAccess->loadByWhere(['user_id' => $_SESSION['user']['id']]);
+        $formIds = array_map(fn($item) => $item['form_id'], $formViewerFromAccess->toArray());
+
+        $forms = new FormsRepo();
+        $forms->loadByWhereQuery('id IN (' . implode(',', $formIds) . ')');
+
+        $menu['tiles'] = [];
+        foreach ($forms as $form) {
+            $temp = [];
+            $temp[$form->name]['url'] = '/form-viewer/form/' . $form->id;
+            $temp[$form->name]['target'] = '_self';
+            $temp[$form->name]['button'] = 'btn-big';
+            $temp[$form->name]['buttonColorClass'] = 'light-purple-1';
+            $temp[$form->name]['icon'] = 'fa-solid fa-file-lines';
+            $temp[$form->name]['iconColor'] = '#A885CC';
+            $menu['tiles'] = array_merge($menu['tiles'], $temp);
+        }
+
+        $tiles = new TilesOnly();
+
         TWIG->render('form_viewer/menu.twig', [
             'adminAccess' => $formViewerSettings->_hasAccess('admin_access', $_SESSION['user']['id']),
+            'content' => $tiles->createTiles(json_encode($menu)),
         ]);
     }
 }
