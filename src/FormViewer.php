629b9a722c7c108834f656af2f549c69c1145c3e
RudyMasGoNext <rudy.mas@go-next.be>
2025-10-20 13:33:56 +0200

    Version 2025.10.20

--- src/FormViewer.php
+++ src/FormViewer.php
@@ -10,7 +10,7 @@ use Twig\Error\LoaderError;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.10.16.1
+ * @version 2025.10.20.0
  * @package Tigress\FormViewer
  */
 class FormViewer
@@ -22,6 +22,6 @@ class FormViewer
      */
     public static function version(): string
     {
-        return '2025.10.16';
+        return '2025.10.20';
     }
 }
