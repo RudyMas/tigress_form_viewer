e24cce7ba4e214805775486b1a0839bd67522d00
RudyMasGoNext <rudy.mas@go-next.be>
2025-12-09 14:16:44 +0100

    Update to PHP 8.5

--- src/FormViewer.php
+++ src/FormViewer.php
@@ -5,12 +5,12 @@ namespace Tigress;
 use Twig\Error\LoaderError;
 
 /**
- * Class FormViewer (PHP version 8.4)
+ * Class FormViewer (PHP version 8.5)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.10.20.0
+ * @version 2025.12.09.0
  * @package Tigress\FormViewer
  */
 class FormViewer
@@ -22,6 +22,6 @@ class FormViewer
      */
     public static function version(): string
     {
-        return '2025.10.20';
+        return '2025.12.09';
     }
 }
